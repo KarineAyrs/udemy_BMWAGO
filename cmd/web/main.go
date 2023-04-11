c761aeefec80869f021b2e693bbaba0ddf351261
KarineAyrs <karine-98@mail.ru>
2023-04-11 22:23:10 +0300

    initial commit, lesson 40

---
+++ cmd/web/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/KarineAyrs/udemyBMWAG/pkg/config"
+	"github.com/KarineAyrs/udemyBMWAG/pkg/handlers"
+	"github.com/KarineAyrs/udemyBMWAG/pkg/render"
+	"log"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+
+}
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,42 @@
+package handlers
+
+import (
+	"github.com/KarineAyrs/udemyBMWAG/pkg/config"
+	"github.com/KarineAyrs/udemyBMWAG/pkg/models"
+	"github.com/KarineAyrs/udemyBMWAG/pkg/render"
+	"net/http"
+)
+
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+// About is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again.."
+
+	// send the data to the template
+	render.Template(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
