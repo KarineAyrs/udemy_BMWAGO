b47404a92d193ef951b318e7d30e1d3ba6c71941
KarineAyrs <karine-98@mail.ru>
2023-04-17 13:15:18 +0300

    add validation

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # Basic Go web application
 
-Project from udemy course ["Building Modern Web Applications with Go"](https://www.udemy.com/course/building-modern-web-applications-with-go/?utm_source=adwords&utm_medium=udemyads&utm_campaign=WebDevelopment_v.PROF_la.EN_cc.ROWMTA-B_ti.8322&utm_content=deal4584&utm_term=_._ag_80869579591_._ad_533999956732_._kw__._de_c_._dm__._pl__._ti_dsa-774930035449_._li_1010561_._pd__._&matchtype=&gclid=CjwKCAjwitShBhA6EiwAq3RqA2Us5UeJf2mg1ZkllcgFhNgF8NePEAoD0p800FmWj48edGZzQ9wXxxoCI3QQAvD_BwE). \
-This repo covers first 5 sections of course:
-- Introduction
-- Overview of the Go language 
-- Building a Basic Web Application 
-- Improved Routing & Middleware
-- State Management with Sessions
+Project from udemy course ["Building Modern Web Applications with Go"](https://www.udemy.com/course/building-modern-web-applications-with-go/?utm_source=adwords&utm_medium=udemyads&utm_campaign=WebDevelopment_v.PROF_la.EN_cc.ROWMTA-B_ti.8322&utm_content=deal4584&utm_term=_._ag_80869579591_._ad_533999956732_._kw__._de_c_._dm__._pl__._ti_dsa-774930035449_._li_1010561_._pd__._&matchtype=&gclid=CjwKCAjwitShBhA6EiwAq3RqA2Us5UeJf2mg1ZkllcgFhNgF8NePEAoD0p800FmWj48edGZzQ9wXxxoCI3QQAvD_BwE).
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/KarineAyrs/udemyBMWAG/internal/config"
+	"github.com/KarineAyrs/udemyBMWAG/internal/forms"
 	"github.com/KarineAyrs/udemyBMWAG/internal/models"
 	"github.com/KarineAyrs/udemyBMWAG/internal/render"
 	"log"
@@ -60,7 +61,47 @@ func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
 }
 
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
+
+	render.Template(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+		Form: forms.New(nil),
+		Data: data,
+	})
+}
+
+func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	reservation := models.Reservation{
+		FirstName: r.Form.Get("first_name"),
+		LastName:  r.Form.Get("last_name"),
+		Email:     r.Form.Get("email"),
+		Phone:     r.Form.Get("phone"),
+	}
+
+	form := forms.New(r.PostForm)
+
+	form.Required("first_name", "last_name", "email")
+	form.MinLength("first_name", 3, r)
+	form.IsEmail("email")
+
+	if !form.Valid() {
+		data := make(map[string]interface{})
+		data["reservation"] = reservation
+
+		render.Template(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+			Form: form,
+			Data: data,
+		})
+		return
+	}
+
 }
 
 func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
